edbd52016e13a7c82fccec5a07b0535a6c8d4b3b
VOHOAITHANH <thanh.vohoai@nidec.com>
2024-06-28 16:53:10 +0700

    push code to the github

---
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        public bool IsLoading  = false;
+
+        public MainViewModel()
+        {
+            if (!IsLoading)
+            {
+                IsLoading = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+
+        }
+    }
+}
